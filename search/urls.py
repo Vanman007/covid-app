eaa3384308c70a8963c3f34f202853d7fc836a55
Vanman007 <tp_vanman@hotmail.com>
2021-01-12 02:09:27 +0100

    removed unused code

--- search/urls.py
+++ search/urls.py
@@ -4,7 +4,6 @@ from . import views as core_views
 app_name = 'search'
 
 urlpatterns = [
-    url(r'^search/$', core_views.search, name='search'),
-
+ 
 
 ]
--- search/views.py
+++ search/views.py
@@ -1,38 +1,3 @@
-from django.contrib.auth import login, authenticate
-from django.shortcuts import render, redirect
-from search.documents import CovidUserDocument
-from elasticsearch_dsl import Search
-from elasticsearch.exceptions import NotFoundError
-from elasticsearch_dsl import Q
-from elasticsearch_dsl import A
-import requests
 
-
-def search(request):
-    city = request.GET.get('citysearch')
-    country = request.GET.get('countrysearch')
-    if city and country:
-        print("wrf")
-        q=(Q("match", city=city) & Q("match", country=country) & Q("match", has_covid=True))
-        result = CovidUserDocument.search().query(q)
-        context ={
-            'country':result.execute().hits[0].country,
-            'city' :result.execute().hits[0].city,
-            'risk':result.execute().hits[0].covid_risk,
-            'hits': result.count()['value']
-            }
-
-    elif country and not city:
-        print(country)
-        result = CovidUserDocument.search().query("match", country__name=country)
-        context ={
-            'country':result.execute().hits[0].country,
-            'risk':result.execute().hits[0].covid_risk,
-            'hits': result.count()['value']
-        }
-    else:
-        context ={}
-
-    return render(request, 'search.html', context)
 
 
